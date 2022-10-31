4316428d732a18b31ba361792507a6410b194f45
alex_kart <alex.kart.23041986@gmail.com>
2022-10-31 11:14:45 +0200

    Lr2

---
+++ main.cpp
@@ -0,0 +1,87 @@
+#include <stm32f10x.h>
+#include <stm32f10x_rcc.h>
+#include <stm32f10x_gpio.h>
+#include <stm32f10x_tim.h>
+
+
+//Oleksii kartushyn
+//Var 16
+//B13, B14 - PWM 0.15C
+//B15 - variable PWM 
+//A0 button - decreate pulse
+//A2 button - increase pulse
+
+#define PERIOD (uint16_t) (SystemCoreClock / 10000)  //Amount of timer ticks for one period
+#define FREQUENT 1  //Amount of periods in one second
+
+int main(void)
+{
+	int i = 0;
+	uint16_t TIM_Pulse;
+	
+	GPIO_InitTypeDef port;
+	TIM_TimeBaseInitTypeDef timer;
+	TIM_OCInitTypeDef timerPWM;
+
+	//Enable clock at GPIOA, GRIOB, and TIM1
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
+
+	//A0 and A2 input mode 
+	GPIO_StructInit(&port);
+	port.GPIO_Mode = GPIO_Mode_IPU;
+	port.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_2;
+	port.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_Init(GPIOA, &port);
+	
+	
+	//B13-B15 push/pull mode
+	GPIO_StructInit(&port);
+	port.GPIO_Mode = GPIO_Mode_AF_PP;
+	port.GPIO_Pin =  GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+	port.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_Init(GPIOB, &port);
+
+	TIM_TimeBaseStructInit(&timer);
+	timer.TIM_Prescaler = SystemCoreClock / PERIOD / FREQUENT;
+	timer.TIM_Period = PERIOD;
+	TIM_TimeBaseInit(TIM1, &timer);
+
+	TIM_Pulse = (uint16_t) (PERIOD*0.85);  //85% to get 15% on inverted pin
+	TIM_OCStructInit(&timerPWM);
+	timerPWM.TIM_OCMode = TIM_OCMode_PWM1;	
+	timerPWM.TIM_OutputNState = TIM_OutputNState_Enable; //CH1N komplimentar output
+	timerPWM.TIM_OutputState = TIM_OutputState_Enable; //CH1 output
+	timerPWM.TIM_Pulse = TIM_Pulse;
+	timerPWM.TIM_OCPolarity = TIM_OCPolarity_High; //CH1 komplimentar output
+	timerPWM.TIM_OCNPolarity = TIM_OCNPolarity_High; //CH1N komplimentar output
+
+	TIM_OC1Init(TIM1, &timerPWM);
+	TIM_OC2Init(TIM1, &timerPWM);
+	TIM_OC3Init(TIM1, &timerPWM);
+
+  TIM_Cmd(TIM1, ENABLE);
+	TIM_CtrlPWMOutputs(TIM1, ENABLE); //turn on MainOutputs. Required for TIM1, not required for others 
+	
+	
+	
+	while (1) {
+		//Button A0 pressed - increase pulse to decrease pulse on inverted pin B15
+		if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0) == 0) {
+			if (TIM_Pulse < PERIOD){
+					TIM_Pulse++;
+					TIM1->CCR3 = TIM_Pulse;
+			}
+		}
+		//Button A2 pressed - decrase pulse to increase pulse on inverted pin B15
+		if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_2) == 0) {
+				if (TIM_Pulse > 0)
+				 TIM_Pulse--;
+				TIM1->CCR3 = TIM_Pulse;
+		}
+		// delay to prevent contact bounce
+		for(i=0;i<0x1000;i++);
+	}
+		
+}
